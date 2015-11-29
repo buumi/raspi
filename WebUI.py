6d2ae99d429781068276e34a15a2332098e8a8c5
Jori Kankaanpää <buhmen@gmail.com>
2015-11-29 23:16:26 +0200

    webbijuttuja v2

--- WebUI.py
+++ WebUI.py
@@ -1,5 +1,5 @@
 import time
-from bottle import route, run, redirect
+from bottle import route, run, redirect, request
 
 class WebUI:
     def __init__(self, ip, lampomittari, valoanturi):
@@ -12,8 +12,8 @@ class WebUI:
     def _route(self):
         route('/', method="GET", callback=self.index)
         route('/tunnistautuminen/<kohdesivu>', method="GET", callback=self.kayttajatunnuksen_pyynto)
-        route('/valoanturin_ohjaus_True', method="GET", callback=self.valoanturin_ohjaus_paalle)
-        route('/valoanturin_ohjaus_False', method="GET", callback=self.valoanturin_ohjaus_pois_paalta)
+        route('/valoanturin_ohjaus_True', method="POST", callback=self.valoanturin_ohjaus_paalle)
+        route('/valoanturin_ohjaus_False', method="POST", callback=self.valoanturin_ohjaus_pois_paalta)
         route('/tiedot', method="GET", callback=self.tieto_sivu)
 
     def kaynnista(self):
@@ -26,11 +26,16 @@ class WebUI:
         return sivu
 
     def kayttajatunnuksen_pyynto(self, kohdesivu):
-        return "<form action='" + kohdesivu + "' method='POST'>Kayttajatunnus: <input type='text' name='kayttajatunnus'><br>Salasana: <input type='password'> name='salasana'><br><input type='submit' value='Jatka'>"
+        return "<form action='" + kohdesivu + "' method='POST'>Kayttajatunnus: <input type='text' name='kayttajatunnus'><br>Salasana: <input type='password' name='salasana'><br><input type='submit' value='Jatka'>"
 
     def valoanturin_ohjaus_paalle(self):
-        self.valoanturi.aseta_ohjauksen_tila(True)
-        redirect("http://" + self.ip + ":8080")
+        username = request.forms.get('kayttajatunnus')
+        password = request.forms.get('salasana')
+        if (self.tarkista_tunnus(username, password)):
+            self.valoanturi.aseta_ohjauksen_tila(True)
+            redirect("http://" + self.ip + ":8080")
+
+
 
     def valoanturin_ohjaus_pois_paalta(self):
         self.valoanturi.aseta_ohjauksen_tila(False)
@@ -38,3 +43,9 @@ class WebUI:
 
     def tieto_sivu(self):
         return "Tasta tulee tietosivu"
+
+    def tarkista_tunnus(self, kayttajatunnus, salasana):
+        if (kayttajatunnus == "Admin" and salasana=="1234"):
+            return True
+        return False
+
