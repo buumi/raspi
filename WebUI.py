d9792ed9ccff596cf3aff0281aca37599cb11411
Jori Kankaanpää <buhmen@gmail.com>
2015-11-30 16:27:28 +0200

    webbijuttuja v2

--- WebUI.py
+++ WebUI.py
@@ -12,9 +12,8 @@ class WebUI:
     def _route(self):
         route('/', method="GET", callback=self.index)
         route('/<toiminto>', method="GET", callback=self.index)
-        route('/tunnistautuminen/<kohdesivu>', method="GET", callback=self.kayttajatunnuksen_pyynto)
-        route('/valoanturin_ohjaus_True', method="POST", callback=self.valoanturin_ohjaus_paalle)
-        route('/valoanturin_ohjaus_False', method="POST", callback=self.valoanturin_ohjaus_pois_paalta)
+        route('/tunnistautuminen/<laite>/<toiminto>', method="GET", callback=self.kayttajatunnuksen_pyynto)
+        route('/<laite>/<toiminto>', method="POST", callback=self.aseta_anturin_ohjaus)
         route('/tiedot', method="GET", callback=self.tieto_sivu)
 
     def kaynnista(self):
@@ -27,34 +26,41 @@ class WebUI:
             sivu += "Kayttajatunnus tai salasana oli vaarin!" + "<br><br>"
 
         uusi_tila_valoanturi = not self.valoanturi.anna_ohjaus_tila()
-        sivu += "Valoanturi ohjaa laitetta: " + str(self.valoanturi.anna_ohjaus_tila()) + " <a href='tunnistautuminen/valoanturin_ohjaus_" + str(uusi_tila_valoanturi) + "'>Muuta</a><br>"
-        sivu += "Lampomittari ohjaa laitetta: " + str(self.lampomittari.anna_ohjaus_tila()) + "<br>"
+        uusi_tila_lampomittari = not self.lampomittari.anna_ohjaus_tila()
+
+        sivu += "Valoanturi ohjaa laitetta: " + str(self.valoanturi.anna_ohjaus_tila()) + " <a href='tunnistautuminen/valoanturi/" + str(uusi_tila_valoanturi) + "'>Muuta</a><br>"
+        sivu += "Lampomittari ohjaa laitetta: " + str(self.lampomittari.anna_ohjaus_tila()) + " <a href='tunnistautuminen/lampomittari/" + str(uusi_tila_lampomittari) + "'>Muuta</a><br>"
         return sivu
 
-    def kayttajatunnuksen_pyynto(self, kohdesivu):
-        return "<form action='/" + kohdesivu + "' method='POST'>Kayttajatunnus: <input type='text' name='kayttajatunnus'><br>Salasana: <input type='password' name='salasana'><br><input type='submit' value='Jatka'>"
+    def kayttajatunnuksen_pyynto(self, laite, toiminto):
+        return "<form action='/" + laite + "/" + toiminto + "' method='POST'>Kayttajatunnus: <input type='text' name='kayttajatunnus'><br>Salasana: <input type='password' name='salasana'><br><input type='submit' value='Jatka'>"
 
-    def valoanturin_ohjaus_paalle(self):
-        username = request.forms.get('kayttajatunnus')
-        password = request.forms.get('salasana')
-        if (self.tarkista_tunnus(username, password)):
-            self.valoanturi.aseta_ohjauksen_tila(True)
+    def aseta_anturin_ohjaus(self, laite, toiminto):
+        if laite == "lampomittari":
+            laite = self.lampomittari
+        elif laite == "valoanturi":
+            laite = self.valoanturi
+        else:
             redirect("http://" + self.ip + ":8080")
-        redirect("http://" + self.ip + ":8080/tunnistautuminen_epaonnistui")
 
+        if toiminto == "True":
+            toiminto = True
+        elif toiminto == "False":
+            toiminto = False
+        else:
+            redirect("http://" + self.ip + ":8080")
 
-    def valoanturin_ohjaus_pois_paalta(self):
         username = request.forms.get('kayttajatunnus')
         password = request.forms.get('salasana')
-        if (self.tarkista_tunnus(username, password)):
-            self.valoanturi.aseta_ohjauksen_tila(False)
+        if (self.onnistuuko_tunnistautuminen(username, password)):
+            laite.aseta_ohjauksen_tila(toiminto)
             redirect("http://" + self.ip + ":8080")
         redirect("http://" + self.ip + ":8080/tunnistautuminen_epaonnistui")
 
     def tieto_sivu(self):
         return "Tasta tulee tietosivu"
 
-    def tarkista_tunnus(self, kayttajatunnus, salasana):
+    def onnistuuko_tunnistautuminen(self, kayttajatunnus, salasana):
         if (kayttajatunnus == "Admin" and salasana=="1234"):
             return True
         return False
